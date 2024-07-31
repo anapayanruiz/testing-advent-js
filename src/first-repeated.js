9d60b3c7aa27840f567d97c6dc8f16439264b0a2
Ana Payan <anapayan22@gmail.com>
2024-07-31 13:14:13 +0200

    refactor: refactor first-advent-js method and install coverage dependencie

--- src/first-repeated.js
+++ src/first-repeated.js
@@ -1,12 +1,30 @@
+/**
+  Si hay más de un número repetido, 
+  debes devolver el número cuya segunda ocurrencia 
+  aparezca primero en la lista. 
+  Si no hay números repetidos, devuelve -1.
+
+  const giftIds = [2, 1, 3, 5, 3, 2]
+  const firstRepeatedId = findFirstRepeated(giftIds)
+  console.log(firstRepeatedId) // 3
+  Aunque el 2 y el 3 se repiten
+  el 3 aparece primero por segunda vez
+
+  const giftIds2 = [1, 2, 3, 4]
+  const firstRepeatedId2 = findFirstRepeated(giftIds2)
+  console.log(firstRepeatedId2) // -1
+  Es -1 ya que no se repite ningún número
+
+  const giftIds3 = [5, 1, 5, 1]
+  const firstRepeatedId3 = findFirstRepeated(giftIds3)
+  console.log(firstRepeatedId3) // 5
+*/
 export const findFirstRepeated = (gifts) => {
   if(!Array.isArray(gifts)) throw new Error('parameter provided must be an array')
+  
   if(gifts.length === 0) throw new Error('parameter provided must be an array with elements')
-
-  const noGiftIds = []
-  gifts.forEach(gift => {
-    if(typeof gift !== 'number') noGiftIds.push(gift)
-  })
-  if(noGiftIds.length > 0) throw new Error('parameter provided must be an array with number elements')
+  
+  if(gifts.find(gift => typeof gift !== 'number')) throw new Error('parameter provided must be an array with number elements')
 
   const uniqueGifts = new Set(gifts)
   if(uniqueGifts.size === gifts.length) return -1
@@ -20,3 +38,4 @@ export const findFirstRepeated = (gifts) => {
   
   return repeatedGifts[0]
 }
+
--- test/first-repeated.test.js
+++ test/first-repeated.test.js
@@ -1,28 +1,6 @@
 import { describe, expect, it } from "vitest"
 import { findFirstRepeated } from "../src/first-repeated"
 
-/* 
-Si hay más de un número repetido, 
-debes devolver el número cuya segunda ocurrencia 
-aparezca primero en la lista. 
-Si no hay números repetidos, devuelve -1.
-
-const giftIds = [2, 1, 3, 5, 3, 2]
-const firstRepeatedId = findFirstRepeated(giftIds)
-console.log(firstRepeatedId) // 3
-// Aunque el 2 y el 3 se repiten
-// el 3 aparece primero por segunda vez
-
-const giftIds2 = [1, 2, 3, 4]
-const firstRepeatedId2 = findFirstRepeated(giftIds2)
-console.log(firstRepeatedId2) // -1
-// Es -1 ya que no se repite ningún número
-
-const giftIds3 = [5, 1, 5, 1]
-const firstRepeatedId3 = findFirstRepeated(giftIds3)
-console.log(firstRepeatedId3) // 5
-*/
-
 describe('findFirstRepeated', () => {
     // it('should be a function', () => {
     //     expect(typeof findFirstRepeated).toBe('function')
@@ -64,3 +42,4 @@ describe('findFirstRepeated', () => {
         expect(findFirstRepeated([2, 8, 1, 8, 8, 2])).toBe(8)
     })
 })
+
